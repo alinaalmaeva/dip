52a7309f0075d3adc3dadb3110b513a92f1dd55e
Alina <almaeva02@mail.ru>
2024-04-23 11:15:10 +0400

    первый

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use('/node_modeles/', express.static(path.join(__dirname, 'node_models')));
+
 
 server.listen(3000);
 app.get('/', function(request, respons){
